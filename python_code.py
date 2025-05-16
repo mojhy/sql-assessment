f06d7b1d2cacbd19e51b854efa50c04b0ba3766b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-16 12:43:39 +1200

    added some defs, no code though

---
+++ python_code.py
@@ -0,0 +1,16 @@
+#made by harrison - numbers go brrrr
+#imports
+import sqlite3
+
+#constants and varible declaration
+DATABASE = "animals.db"
+
+# functions
+def add_animals():
+    '''adds an animal'''
+
+def select_by_ranking():
+    '''selects * by the ranking'''
+
+def select_all():
+    '''selects all readible data'''
