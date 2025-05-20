4539fb6515589674b46df133149c4534fec42dc9
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-20 12:45:28 +1200

    added some stufff that is very useful

--- python_code.py
+++ python_code.py
@@ -1,4 +1,6 @@
 #made by harrison - animals go brrrr
+#data was taken from reddit btw
+#all raings were made on the assumption that i have a small knife
 #imports
 import sqlite3
 
@@ -19,23 +21,38 @@ def get_stuff(query):
 
 def add_animals():
     '''adds an animal'''
-    get_stuff(f"INSERT INTO ANIMALS ( animal_name, scientific_name, animal_info, could_i_take_it_in_a_fight ) VALUES ({input('animal name')}, {input('scientific name')}, {input('group')}, {input('ranking, 81 to 85, 81 is death')});")
+    get_stuff(f"INSERT INTO ANIMALS ( animal_name, scientific_name, animal_info, could_i_take_it_in_a_fight ) VALUES ({input('animal name: ')}, {input('scientific name: ')}, {input('group: ')}, {input('ranking, 81 to 85, 81 is death')});")
 
 def select_by_ranking():
     '''selects * by the ranking'''
-    search = input('what ranking? \n1. im COOKED \n 2. 1 percent chance I dont lose \n 3. ill probably lose \n4. I definitely have a chance \n5. Im whooping their ASS \n')
-    
+    search = int(input('what ranking? \n1. im COOKED \n2. 1 percent chance I dont lose \n3. ill probably lose \n4. I definitely have a chance \n5. Im whooping their ASS \n'))    
+    if search == 1:
+        get_stuff('SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id where could_i_take_it_in_a_fight = 81;')
+    elif search == 2:
+        get_stuff('SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id where could_i_take_it_in_a_fight = 82;')
+    elif search == 3:
+        get_stuff('SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id where could_i_take_it_in_a_fight = 83;')
+    elif search == 4:
+        get_stuff('SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id where could_i_take_it_in_a_fight = 84;')
+    elif search == 5:
+        get_stuff('SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id where could_i_take_it_in_a_fight = 85;')
 
 def select_all():
     '''selects all readible data'''
     get_stuff('SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id;')
+
+# main code
+run = input('what do you want to do? ')
+while run.isnumeric() or not run.isnumeric():
     
-run = int(input('what do you want to do? '))
-if run == 1:
-    add_animals()
-elif run == 2:
-    select_by_ranking()
-elif run  == 3:
-    select_all()
-else:
-    print('no')
+    if run == '1':
+        add_animals()
+    elif run == '2':
+        select_by_ranking()
+    elif run  == '3':
+        select_all()
+    elif run == 'exit':
+        break
+    else:
+        print('no')
+    run = input('what do you want to do? ')
