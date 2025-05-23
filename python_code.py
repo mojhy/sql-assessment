4382288bc83cff6fc58d2546391b3d3778683dcf
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-23 13:31:14 +1200

    added colour

--- python_code.py
+++ python_code.py
@@ -3,7 +3,17 @@
 #all ratings were made on the assumption that i have a small knife
 #imports
 import sqlite3
-
+#colour declaraions
+r = '\033[31m'
+g = '\033[32m'
+y = '\033[33m'
+b = '\033[34m'
+reset = '\033[0m'
+br = '\033[41m'
+bg = '\033[42m'
+by = '\033[43m'
+bb = '\033[44m'
+bp = '\033[45m'
 #constants and varible declaration
 DATABASE = "animals"
 
@@ -22,29 +32,27 @@ def get_stuff(query):
     results = cursor.fetchall()
     for number in results:
         print(f"     {number[0]:<5}{number[1]:<40}{number[2]:<35} {number[4]:<15} {number[3]}")
-    
+ 
 def ranking_nums():
-    print('from 81 being death, to 85 being easy as hell, ')
-    print('81, 82, 83, 84, 85,')
+    print(f'{y}from 81 being death, to 85 being easy as hell, {reset}')
+    print(f'{y}81, 82, 83, 84, 85,{reset}')
 def group_names():
     '''prints group names'''
-    print(' 86	Reptile,\n 87	Mammal,\n 88	Insect,\n 89	Arachnid,\n 90	Amphibian,\n 91	Bird,\n 92	Mollusk,\n 93	Cockroach,\n 94	Myriapod')
+    print(f'{y}86	Reptile,\n 87	Mammal,\n 88	Insect,\n 89	Arachnid,\n 90	Amphibian,\n 91	Bird,\n 92	Mollusk,\n 93	Cockroach,\n 94	Myriapod{reset}')
 
 def add_animals():
     '''adds an animal'''
-    animal_name = input('animal name: ')
-    scientific_name = input('scientific name: ')
+    animal_name = input(f'{bb}animal name: {reset}')
+    scientific_name = input(f'{bb}scientific name: {reset}')
     group_names()
-    group = int(input('group: '))
-    ranking = int(input('ranking, 81 to 85, 81 is death: '))
+    group = int(input(f'{bb}group: {reset}'))
+    ranking = int(input(f'{bb}ranking, 81 to 85, 81 is death: {reset}'))
     qurey = f"INSERT INTO ANIMALS (animal_name, scientific_name, animal_info, could_i_take_it_in_a_fight) VALUES('{animal_name.title()}', '{scientific_name.title()}', '{group}', '{ranking}');"
     cursor.execute(qurey,)
-    
-    
 
 def select_by_ranking():
     '''selects * by the ranking'''
-    search = int(input('what ranking? \n1. im COOKED \n2. 1 percent chance I dont lose \n3. ill probably lose \n4. I definitely have a chance \n5. Im whooping their ASS \n'))    
+    search = int(input(f'{y}1. im COOKED \n2. 1 percent chance I dont lose \n3. ill probably lose \n4. I definitely have a chance \n5. Im whooping their ASS \n{bb}what ranking? \n{reset}'))    
     if search == 1:
         ranking_help(81)
     elif search == 2:
@@ -62,29 +70,29 @@ def select_all():
 
 def search_animal():
     '''search for a specific animal'''
-    animal = input('what animal would you like to search for? ').title()
+    animal = input(f'{bb}what animal would you like to search for? {reset}').title()
     get_stuff(f"select * from ANIMALS where animal_name = '{animal}';")
 
 def kill_an_animal():
-    animal = input('what animal would you like to kill? ').title()
+    animal = input(f'{bb}what animal would you like to kill? {reset}').title()
     query = f'Delete from animals where animal_name = {animal.title()};'
     cursor.execute(query, )
 
 def edit_an_animal():
-    print('DO NOT CHANGE ANIMAL idS!!!!!!!!!!!!!!!')
-    change = input('what animal do you want to change? ')
-    print('columms are; \nanimal_name\nscientific_name\ncould_i_take_it_in_a_fight\nanimal_info(group)')
-    columm = input('what columm do you want to edit? ')
+    print(f'{br}DO NOT CHANGE ANIMAL idS!!!!!!!!!!!!!!!{reset}')
+    change = input(f'{bb}what animal do you want to change? {reset}')
+    print(f'{y}columms are; \nanimal_name\nscientific_name\ncould_i_take_it_in_a_fight\nanimal_info(group){reset}')
+    columm = input(f'{bb}what columm do you want to edit? {reset}')
     if columm == 'could_i_take_it_in_a_fight':
         ranking_nums()
     if columm == 'group':
         group_names()
-    changes = input("what do you want to change it to? ")
+    changes = input(f"{bb}what do you want to change it to? {reset}")
     query = f"UPDATE animals SET {columm} = '{changes}' WHERE animal_name = '{change}';"
     cursor.execute(query,)
 
 def advanced_():
-    cursor.execute(input('Write your own query: '))
+    cursor.execute(input(f'{bb}Write your own query: {reset}'))
     results = cursor.fetchall()
     for number in results:
         print(f"     {number}")
@@ -95,7 +103,7 @@ print(options)
 run = input('what do you want to do? (enter a number) ')
 while run.isnumeric() or not run.isnumeric():
     
-    if run == '1':
+    if   run == '1':
         add_animals()
     elif run == '2':
         select_by_ranking()
@@ -112,7 +120,7 @@ while run.isnumeric() or not run.isnumeric():
     elif run == '7':
         advanced_()
     else:
-        print('no')
+        print(f'{r}no{reset}')
     
     print(options)
     run = input('what do you want to do? ')
