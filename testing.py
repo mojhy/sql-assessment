03cbe58dde10e6d5bc07ddfb710218397748eebf
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-26 10:10:19 +1200

    idk what im doin

---
+++ testing.py
@@ -0,0 +1,20 @@
+r = '\033[31m'
+g = '\033[32m'
+y = '\033[33m'
+b = '\033[34m'
+reset = '\033[0m'
+br = '\033[41m'
+bg = '\033[42m'
+by = '\033[43m'
+bb = '\033[44m'
+bo = '\033[46m'
+
+print(f"{y}tots{reset}")
+print(f"{r}tots{reset}")
+print(f"{b}tots{reset}")
+print(f"{g}tots{reset}")
+print(f"{br}tots{reset}")
+print(f"{bg}tots{reset}")
+print(f"{by}tots{reset}")
+print(f"{bb}tots{reset}")
+print(f"{bo}tots{reset}")
