5de653a0640f23ebb05e14584bc9533fbb7ac68e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-29 14:34:01 +1200

    added about 40 comments

--- python_code.py
+++ python_code.py
@@ -19,8 +19,8 @@ bp = '\033[45m'
 DATABASE = "animals"
 
 options = f'{y}1. add an animal\n2. select by ranking\n3. select all\n4. search for an animal\n5. kill an animal\n6. edit an animal\n7. advanced\nexit{reset}'
-db = sqlite3.connect('animals')
-cursor = db.cursor()
+db = sqlite3.connect('animals') # connects to my database
+cursor = db.cursor() # makes future code more efficient
 
 # functions
 def exit_check(check):
@@ -28,10 +28,11 @@ def exit_check(check):
     if check.lower() == "exit":
         return True
 
-def ranking_help(value):
+def ranking_help(value): # this function is only used in 5 instinces, and only exists because sriram and mahi were mad at me
     """helps"""
     get_stuff(f'SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id where could_i_take_it_in_a_fight = {value};')
-def get_stuff(query):
+
+def get_stuff(query): # this function runs and prints a query so i dont have to write the same block of code a buch of times
     """get stuff"""
     sql = query
     cursor.execute(sql,)
@@ -39,40 +40,41 @@ def get_stuff(query):
     headers = [description[0] for description in cursor.description]
     print(tabulate(results, headers=headers, tablefmt='github'))
 
-def ranking_nums():
+def ranking_nums(): # this function just prints some numbers
     """prints some number"""
     print(f'{y}from 81 being death, to 85 being easy as hell, {reset}')
     print(f'{y}81, 82, 83, 84, 85,{reset}')
 
-def group_names():
+def group_names(): # this function prints out the group names so i dont have to write it out a bunch of times
     """print the group names"""
-    '''prints group names'''
     print(f'{y} 86	Reptile,\n 87	Mammal,\n 88	Insect,\n 89	Arachnid,\n 90	Amphibian,\n 91	Bird,\n 92	Mollusk,\n 93	Cockroach,\n 94	Myriapod{reset}')
 
-def add_animals():
+def add_animals(): # this function lets the user add a animal
     '''adds an animal'''
-    animal_name = input(f'{bb}animal name: {reset}')
-    if exit_check(animal_name):
+    animal_name = input(f'{bb}animal name: {reset}') # animal name
+    if exit_check(animal_name): # checks if the varible is an exit thingamiboba
         return
-    scientific_name = input(f'{bb}scientific name: {reset}')
-    if exit_check(scientific_name):
+    scientific_name = input(f'{bb}scientific name: {reset}') # takes the scientific name of the animal being added
+    if exit_check(scientific_name): # checks if the varible is an exit thingamiboba
         return
     group_names()
-    group = input(f'{bb}group: {reset}')
-    if exit_check(group):
+    group = input(f'{bb}group: {reset}') # takes the group on the animal being added
+    if exit_check(group): # checks if the varible is an exit thingamiboba
         return
-    ranking = input(f'{bb}ranking, 81 to 85, 81 is death: {reset}')
-    if exit_check(ranking):
+    ranking = input(f'{bb}ranking, 81 to 85, 81 is death: {reset}') # takes the ranking of the animal being added
+    if exit_check(ranking): # checks if the varible is an exit thingamiboba
         return
-    qurey = f"INSERT INTO ANIMALS (animal_name, scientific_name, animal_info, could_i_take_it_in_a_fight) VALUES('{animal_name.title()}', '{scientific_name.title()}', '{int(group)}', '{int(ranking)}');"
-    cursor.execute(qurey,)
+    # this is the query that will be executed
+    qurey = f"INSERT INTO ANIMALS (animal_name, scientific_name, animal_info, could_i_take_it_in_a_fight) VALUES('{animal_name.title()}', '{scientific_name.title()}', '{int(group)}', '{int(ranking)}');" 
+    cursor.execute(qurey,) # thuis executes the query
 
-def select_by_ranking():
+def select_by_ranking(): # this function will print all animals of 1 ranking
     '''select * by the ranking'''
     search = input(f'{y}1. im COOKED \n2. 1 percent chance I dont lose \n3. ill probably lose \n4. I definitely have a chance \n5. Im whooping their ASS \n{bb}what ranking? \n{reset}')
-    if exit_check(search):
+    if exit_check(search): # checks if the varible is an exit thingamiboba
         return
-    if   int(search) == 1:
+    # this if statment is here to search for the thing and i know it looks bad but it was WAY worse
+    if int(search) == 1:
         ranking_help(81)
     elif int(search) == 2:
         ranking_help(82)
@@ -83,49 +85,49 @@ def select_by_ranking():
     elif int(search) == 5:
         ranking_help(85)
 
-def select_all():
+def select_all(): # this function just selects all the readible data in the database in a nice looking way
     '''selects all readible data'''
     get_stuff('SELECT ANIMALS.animal_id, ANIMALS.animal_name, ANIMALS.scientific_name, RANKING.could_i_take_it AS fight_rating, INFO."group" AS animal_group FROM ANIMALS JOIN RANKING ON ANIMALS.could_i_take_it_in_a_fight = RANKING.rank_id JOIN INFO ON ANIMALS.animal_info = INFO.info_id;')
 
-def search_animal():
+def search_animal(): # this funtion searches for a specific animal by their name
     '''search for a specific animal'''
     animal = input(f'{bb}what animal would you like to search for? {reset}').title()
-    if exit_check(animal):
+    if exit_check(animal): # checks if the varible is an exit thingamiboba
         return
     get_stuff(f"select * from ANIMALS where animal_name = '{animal}';")
 
-def kill_an_animal():
+def kill_an_animal(): # this function deleates an animal from the database
     """kills an animal"""
-    animal = input(f'{bb}what animal would you like to kill? {reset}').title()
-    if exit_check(animal):
+    animal = input(f'{bb}what animal would you like to kill? {reset}').title() # this inputs the animal that is going to be deleated
+    if exit_check(animal): # checks if the varible is an exit thingamiboba
         return
     query = f'Delete from animals where animal_name = {animal.title()};'
     cursor.execute(query, )
 
-def edit_an_animal():
+def edit_an_animal(): # this function lets the user edit an animal
     """edits an animal"""
     print(f'{br}DO NOT CHANGE ANIMAL idS!!!!!!!!!!!!!!!{reset}')
-    change = input(f'{bb}what animal do you want to change? {reset}')
-    if exit_check(change):
+    change = input(f'{bb}what animal do you want to change? {reset}') # this inputs the animal that the user wants to change
+    if exit_check(change): # checks if the varible is an exit thingamiboba
         return
     print(f'{y}columms are; \nanimal_name\nscientific_name\ncould_i_take_it_in_a_fight\nanimal_info(group){reset}')
-    columm = input(f'{bb}what columm do you want to edit? {reset}')
-    if exit_check(columm):
+    columm = input(f'{bb}what columm do you want to edit? {reset}') # these are the columms
+    if exit_check(columm): # checks if the varible is an exit thingamiboba
         return
     if columm == 'could_i_take_it_in_a_fight':
         ranking_nums()
     if columm == 'group':
         group_names()
     changes = input(f"{bb}what do you want to change it to? {reset}")
-    if exit_check(changes):
+    if exit_check(changes): # checks if the varible is an exit thingamiboba
         return
     query = f"UPDATE animals SET {columm} = '{changes}' WHERE animal_name = '{change}';"
     cursor.execute(query,)
 
-def advanced_():
+def advanced_(): # i added this because i was bored and the data isnt live anyway so it doesnt matter too much if they deleate the database
     """write ur own query"""
     Qss = input(f'{bb}Write your own query: {reset}')
-    if exit_check(Qss):
+    if exit_check(Qss): # checks if the varible is an exit thingamiboba
         return
     cursor.execute(Qss)
     results = cursor.fetchall()
@@ -155,7 +157,7 @@ while run.isnumeric() or not run.isnumeric():
     elif run == '7':
         advanced_()
     else:
-        print(f'{r}no{reset}')
+        print(f'{r}nthat is not an option{reset}')
     
     print(options)
     run = input(f'{bb}what do you want to do? {reset}')
